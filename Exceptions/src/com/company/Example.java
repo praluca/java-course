c047b75bee2425f781528bc6eb993b3a07d31000
unknown <poparaluca98@yahoo.com>
2020-08-28 12:46:24 +0300

    examples about throwing and catching exceptions

---
+++ Exceptions/src/com/company/Example.java
@@ -0,0 +1,46 @@
+package com.company;
+
+import java.util.InputMismatchException;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
+public class Example {
+    public static void main(String[] args) {
+        try{
+            int result = divided();
+            System.out.println(result);
+        }catch (ArithmeticException | NoSuchElementException e) {
+            System.out.println(e.toString());
+            System.out.println("Unable to perform division, autopilot shutting down");
+        }
+
+    }
+    private static int divided() {
+        int x, y;
+//        try{
+            x = getInt();
+            y = getInt();
+            System.out.println("x is " + x + ", y is " + y);
+            return x / y;
+//        }catch (NoSuchElementException e) {
+//            throw  new ArithmeticException("no suitable input");
+//        } catch (ArithmeticException e) {
+//            throw  new ArithmeticException("attempt to divide by zero");
+//        }
+
+    }
+
+    private static int getInt() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter an integer: ");
+        while (true) {
+            try{
+                return scanner.nextInt();
+            }catch (InputMismatchException e) {
+                scanner.nextLine();
+                System.out.println("Please enter a number using only the digits 0 to 9");
+            }
+        }
+
+    }
+}
